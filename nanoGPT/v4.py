46f83b3e292e6470409b011fc85d2c0bbf7fcbb9
Oleg Gera <hi@oleggera.com>
2024-01-13 20:54:23 +0000

    feat: multi head attention

--- nanoGPT/v4.py
+++ nanoGPT/v4.py
@@ -94,6 +94,15 @@ class Head(nn.Module):
         return out
 
 
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
 # Bigram model
 
 
@@ -102,7 +111,7 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        self.sa_head = Head(n_embed)
+        self.sa_head = MultiHeadAttention(4, n_embed // 4)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
