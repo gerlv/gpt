bf37579ad020b1c2d5650abde5dd73c3a04f4a66
Oleg Gera <hi@oleggera.com>
2024-01-13 19:59:50 +0000

    chore: bigram complete, issue with max new tokens

--- nanoGPT/bigram_v2.py
+++ nanoGPT/bigram_v2.py
@@ -8,10 +8,11 @@ torch.manual_seed(1337)
 batch_size = 32
 block_size = 8
 training_split = 0.9
-steps = 10000
-max_new_tokens = 100
+steps = 1000
+max_new_tokens = 7
 eval_iters = 200
 device = 'cpu'
+n_embed = 32
 
 dataset = Path('../data/tinyshakespear.txt')
 text = dataset.read_text()
@@ -47,12 +48,14 @@ val_data = data[n:]  # 10%
 # A: torch.stack is a stack of multiple individual rows, so randint returns a stack of
 #    multiple random values. And you can iterate on multiple values straight away
 
+
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size, ))
     x = torch.stack([data[i: i + block_size] for i in ix])
     y = torch.stack([data[i + 1: i + block_size + 1] for i in ix])
     return x.to(device), y.to(device)
+
 
 @torch.no_grad()
 def estimate_loss():
@@ -73,12 +76,22 @@ def estimate_loss():
 
 
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size: int):
+    def __init__(self):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.lm_head = nn.Linear(n_embed, vocab_size)
 
-    def forward(self, idx, targets = None):
-        logits = self.token_embedding_table(idx)  # (B,T,C) - batch, time, vocab size
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        print(B, T)
+
+        tok_emb = self.token_embedding_table(idx)  # (B, T, n_embed)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  # (T, C)
+        x = tok_emb + pos_emb  # (B, T, C)
+        # x holds token identities and positions where these tokens occur
+        logits = self.lm_head(x)  # (B, T, vocab_size) - batch, time, vocab size
+
         # logits are scores for the next token, we are predicting what comes next
         # based on single token
 
@@ -111,10 +124,9 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel(vocab_size=vocab_size)
+model = BigramLanguageModel()
 m = model.to(device=device)
 
-idx = torch.zeros((1, 1), dtype=torch.long, device=device)
 loss = None
 
 # train the model
@@ -134,4 +146,5 @@ for step in range(steps):
     optimizer.step()
 
 print("Bigram Loss: ", loss.item())
+idx = torch.zeros((1, 1), dtype=torch.long, device=device)
 print(decode(m.generate(idx=idx, max_new_tokens=max_new_tokens)[0].tolist()))
